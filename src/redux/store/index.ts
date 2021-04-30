f7683808393643d04da9b2ccaf75604236d1ff2c
Aitor <aitorpalomares@hotmail.com>
2021-04-30 15:49:00 +0100

    add thunk to redux

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,16 +1,13 @@
-// @ts-ignore
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from "../reducers";
-
-const registerReduxDevtools = () => {
-  const w: any = window;
-  return w.__REDUX_DEVTOOLS_EXTENSION__ && w.__REDUX_DEVTOOLS_EXTENSION__()
-}
 
 const store = createStore(
   rootReducer,
-  registerReduxDevtools()
-// +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
 );
 
 export default store;
