04fa204f33be6e39201179be0451798b87457632
Aitor <aitorpalomares@hotmail.com>
2021-05-03 12:16:20 +0100

    get updated vaues with debounce

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Board } from '../components/Board';
 import { BtnReset } from '../components/BtnReset';
+import useDebounce from '../hooks/useDebounce';
+import { Props } from './App.interfaces';
 import styles from './App.module.scss';
 
-const App = () => (
-  <div className={styles.App}>
-    <div className={styles.content}>
-      <Board />
-      <BtnReset />
+const App = ({ tiles }: Props) => {
+  const savedTiles = useDebounce(tiles, 2000);
+
+  useEffect(() => {
+    console.log('aaaa', savedTiles)
+  }, [savedTiles])
+
+  return (
+    <div className={styles.App}>
+      <div className={styles.content}>
+        <Board />
+        <BtnReset />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
