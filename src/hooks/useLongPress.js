a62eb20cc970683fad7d9ede53e82f9c394d13a8
Aitor <aitorpalomares@hotmail.com>
2021-05-03 11:46:34 +0100

    update delays for longPress

--- src/hooks/useLongPress.js
+++ src/hooks/useLongPress.js
@@ -7,7 +7,7 @@ const useLongPress = (
   },
   {
     shouldPreventDefault = true,
-    delay = 500,
+    delay = 300,
   } = {}
 ) => {
   const [longPressTriggered, setLongPressTriggered] = useState(false);
