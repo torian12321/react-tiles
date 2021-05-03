2e12de6c1953b031de710e0d44015f1aadde6a39
Aitor <aitorpalomares@hotmail.com>
2021-05-03 10:05:23 +0100

    relocate files

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import './index.css';
 import App from './App';
 import store from './redux/store'
 import reportWebVitals from './reportWebVitals';
+import './assets/styles/index.scss';
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById('🦄')
 
 ReactDOM.render(
   <React.StrictMode>
